74e4a7777a6a4fc8e79175d47eb2a54dbce3853a
Juan I. Benavides <linnk87@gmail.com>
2018-06-14 15:25:15 -0500

    Better description of my previous jobs and vertical alignment.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Linnk</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
         <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
@@ -14,42 +14,83 @@
                     <header>
                 		<h1>Hello</h1>
                 	</header>
-                    <p>I'm Ignacio Benavides, a Software Engineer from Guadalajara, México.</p>
-                    <p>Currently working at <a target="_blank" href="https://incrementacrm.com/">Incrementa</a> as CTO / <span title="I hate the term serial-entreperneur, but I think I'm one of those.">Cofounder</span> doing software development, product design and project management.</p>
-                    <p>I love coding using Node.js, Swift and CakePHP, mostly.</p>
+                    <p>
+                        I'm <strong>Ignacio Benavides</strong>, a <em>Software Engineer</em> based in Guadalajara,
+                        México.
+                    </p>
+                    <p>
+                        I love creating useful things that people would want to use. I do it mostly using languages
+                        like <em>Javascript</em>, <em>Swift</em> and <em title="ʕ •ᴥ•ʔ">PHP</em>. Currently, I work at
+                        <a target="_blank" href="https://incrementacrm.com/">Incrementa</a> where I do a bit of
+                        everything: leading, coding, product design, management and even customer support. :)
+                    </p>
                 </article>
             </section>
 
             <section id="experience">
+                <h2>Experience</h2>
                 <article>
-                    <div class="company"><a target="_blank" href="https://incrementacrm.com/">Incrementa</a></div>
-                    <div class="date">2013 - Currently</div>
-                    <div class="description"><em>Cofounder</em> and <em>CTO</em> at the best CRM solution for Latin America businesses.</div>
+                    <div class="company">
+                        <p><a target="_blank" href="https://incrementacrm.com/">Incrementa</a></p>
+                        <p class="date">2013 - Currently</p>
+                    </div>
+                    <div class="description">
+                        <em title="I hate the term serial-entreperneur, but I think I'm one of those.">Cofounder</em>
+                        and <em>CTO</em> for a CRM cloud based application, designed specifically for Latina America’s
+                        small businesses. We currently have over 100 premium accounts and serve around 500 active
+                        users every day. 
+                    </div>
                 </article>
                 <article>
-                    <div class="company"><a target="_blank" href="https://spumer.com">Spumer</a></div>
-                    <div class="date">2014 - Currently</div>
-                    <div class="description"><em>Cofounder</em> and <em>CTO</em> for a web chat for businesses.</div>
+                    <div class="company">
+                        <p><a target="_blank" href="https://spumer.com">Spumer</a></p>
+                        <p class="date">2014 - Currently</p>
+                    </div>
+                    <div class="description">
+                        <em>Cofounder</em> and <em>CTO</em> for a web chat for businesses. Started as an experiment;
+                        we’re currently using it on production. 
+                    </div>
                 </article>
                 <article>
-                    <div class="company"><a target="_blank" href="http://buskeeper.com">Buskeeper</a></div>
-                    <div class="date">2013 - Currently</div>
-                    <div class="description"><em>Cofounder</em> and <em>mobile developer</em> at my side non-profit project.</div>
+                    <div class="company">
+                        <p><a target="_blank" href="http://buskeeper.com">Buskeeper</a></p>
+                        <p class="date">2013 - 2015</p>
+                    </div>
+                    <div class="description">
+                        <em>Founder</em> and <em>mobile developer</em> at my side non-profit project along with my
+                        <em>brother</em>. It was an iOS app that can mesure the quality of public transportation.
+                    </div>
                 </article>
                 <article>
-                    <div class="company"><a target="_blank" href="https://wem.mx">WEM Creative Studio</a></div>
-                    <div class="date">2009 - 2012</div>
-                    <div class="description"><em>Cofounder</em> and <em>web / mobile developer</em> at a Digital Studio. We made the first iPad App for México.</div>
+                    <div class="company">
+                        <p><a target="_blank" href="https://wem.mx">WEM Creative Studio</a></p>
+                        <p class="date">2009 - 2012</p>
+                    </div>
+                    <div class="description">
+                        <em>Cofounder</em> at a digital agency. We made a lot of things for clients such as design,
+                        web apps and iOS apps, including the first (probably) iPad App in México.
+                    </div>
                 </article>
                 <article>
-                    <div class="company">Freelance</div>
-                    <div class="date">2008 - 2009</div>
-                    <div class="description">Development, DevOps and security consultant. <em>I met my Incrementa cofounder in one of these jobs</em>.</div>
+                    <div class="company">
+                        <p>Freelance</p>
+                        <p class="date">2008 - 2009</p>
+                    </div>
+                    <div class="description">
+                        Part-time development, linux admin and security consultant; I did this as a way of earn
+                        something while going to college.
+                    </div>
                 </article>
                 <article>
-                    <div class="company"><a target="_blank" href="https://www.eskalon.com">Web developer at Eskalon</a></div>
-                    <div class="date">2006 - 2008</div>
-                    <div class="description"><em title=":D">My first «programming job»</em> as web developer for a local Web Development Agency.</div>
+                    <div class="company">
+                        <p><a target="_blank" href="https://www.eskalon.com">Web developer at Eskalon</a></p>
+                        <p class="date">2006 - 2008</p>
+                    </div>
+                    <div class="description">
+                        <em title=":D">My first «programming job»</em> as web developer for a small Web Design Agency.
+                        I learned Linux around here. Also, this is when we fought the holy war against Internet
+                        Explorer.
+                    </div>
                 </article>
             </section>
         </main>
